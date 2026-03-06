c4e2ad27a8fc52d034026ca1cf1ab7b62f5a74dd
dgTr3 <Edugta31@gmail.com>
2026-03-05 20:23:43 -0600

    completo

--- Do-While10/Program.cs
+++ Do-While10/Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Del 1 al 50");
+int i = 1;
+int conta = 0;
+do
+{
+   if (i % 2 == 0) {
+        Console.WriteLine(i);
+        conta++;
+    }
+    i++;
+} while (i <= 50);
+Console.WriteLine("Cantidad de números: " + conta);
